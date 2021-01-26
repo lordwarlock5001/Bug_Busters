234c3fdd435b16da21267c4ffddafd5ec4aca1dd
Rushikesh Kamble <65526768+lordwarlock5001@users.noreply.github.com>
2021-01-26 09:52:39 +0530

    car detection code added

--- smart traffic system/detect_cars.py
+++ smart traffic system/detect_cars.py
@@ -1,32 +1,17 @@
+from imageai.Detection import VideoObjectDetection,ObjectDetection
+import os
 import cv2
-class Detect_cars:
-    def __init__(self):
-        self.cap=cv2.VideoCapture("video/test_footage.mp4")
-        self.car_cass=cv2.CascadeClassifier('cars.xml')
-
-    def detect(self):
-        while True:
-            ret, frames = self.cap.read()
-
-            # convert to gray scale of each frames
-            gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
-
-            # Detects cars of different sizes in the input image
-            cars = self.car_cass.detectMultiScale(gray, 1.1, 1)
-
-            # To draw a rectangle in each cars
-            for (x, y, w, h) in cars:
-                cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-                # Display frames in a window
-            cv2.imshow('video2', frames)
-            #cv2.waitKey(1)
-            # Wait for Esc key to stop
-            if cv2.waitKey(33) == 27:
-                break
-
-    # De-allocate any associated memory usage
-    cv2.destroyAllWindows()
-
-obj=Detect_cars()
-obj.detect()
+# Get current working directory
+curDir = os.getcwd()
+# Initialize video capture (webcam)
+camera = cv2.VideoCapture("video/test_footage.mp4")
+# Initialize imageAI objects and files
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(curDir, "yolo.h5"))
+detector.loadModel()
+# Start object detection and print frames
+video_path = detector.detectObjectsFromVideo(camera_input=camera,
+                                output_file_path=os.path.join(curDir, "cam1")
+                                , frames_per_second=4, log_progress=True)
+print(video_path)
