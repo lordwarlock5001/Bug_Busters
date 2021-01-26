c95a8fff7a667a3777cda5b40e15024c8f09c253
Rushikesh Kamble <65526768+lordwarlock5001@users.noreply.github.com>
2021-01-26 13:27:02 +0530

    deleted line.py

--- smart traffic system/detect_cars.py
+++ smart traffic system/detect_cars.py
@@ -9,17 +9,20 @@ class Detect_cars:
         self.detector=ObjectDetection()
         self.detector.setModelTypeAsYOLOv3()
         self.detector.setModelPath(os.path.join(self.curD,"yolo.h5"))
+        self.cust=self.detector.CustomObjects(car=True,motorcycle=True,truck=True)
         self.detector.loadModel()
+
     def detect(self):
         while True:
             suc,frame=self.footage.read()
-            image_path=self.detector.detectObjectsFromImage(input_image=frame,input_type="array",output_image_path="image.jpg")
+
+            image_path=self.detector.detectObjectsFromImage(custom_objects=self.cust,display_percentage_probability=False,display_object_name=False,input_image=frame,input_type="array",output_image_path="image.jpg")
+            print(len(image_path))
             image=cv2.imread("image.jpg")
             cv2.imshow("image",image)
             k = cv2.waitKey(1) & 0xFF
             # press 'q' to exit
             if k == ord('q'):
                 break
-                print(k)
 obj=Detect_cars()
 obj.detect()
