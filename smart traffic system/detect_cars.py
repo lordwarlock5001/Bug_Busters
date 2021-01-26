3347fe4e5f25270e8c5663f04a098efda107e433
Rushikesh Kamble <65526768+lordwarlock5001@users.noreply.github.com>
2021-01-26 10:41:12 +0530

    wrapped in class

--- smart traffic system/detect_cars.py
+++ smart traffic system/detect_cars.py
@@ -1,17 +1,25 @@
 from imageai.Detection import VideoObjectDetection,ObjectDetection
 import os
 import cv2
-# Get current working directory
-curDir = os.getcwd()
-# Initialize video capture (webcam)
-camera = cv2.VideoCapture("video/test_footage.mp4")
-# Initialize imageAI objects and files
-detector = VideoObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(curDir, "yolo.h5"))
-detector.loadModel()
-# Start object detection and print frames
-video_path = detector.detectObjectsFromVideo(camera_input=camera,
-                                output_file_path=os.path.join(curDir, "cam1")
-                                , frames_per_second=4, log_progress=True)
-print(video_path)
+import numpy as np
+class Detect_cars:
+    def __init__(self):
+        self.curD=os.getcwd()
+        self.footage=cv2.VideoCapture("video/test_footage.mp4")
+        self.detector=ObjectDetection()
+        self.detector.setModelTypeAsYOLOv3()
+        self.detector.setModelPath(os.path.join(self.curD,"yolo.h5"))
+        self.detector.loadModel()
+    def detect(self):
+        while True:
+            suc,frame=self.footage.read()
+            image_path=self.detector.detectObjectsFromImage(input_image=frame,input_type="array",output_image_path="image.jpg")
+            image=cv2.imread("image.jpg")
+            cv2.imshow("image",image)
+            k = cv2.waitKey(1) & 0xFF
+            # press 'q' to exit
+            if k == ord('q'):
+                break
+                print(k)
+obj=Detect_cars()
+obj.detect()
