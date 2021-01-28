7754d38e567500a35398bdb49555ef4c8990ef59
riyashelwante <77475914+riyashelwante@users.noreply.github.com>
2021-01-28 21:16:39 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.riya.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+                startActivity(new Intent(MainActivity.this,Welcome.class));
+
+
+    }
+}
